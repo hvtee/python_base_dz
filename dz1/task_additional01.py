a81cd4243b2441004e129e491fab2d7a1715e7a2
Artem <milkartem2@mail.ru>
2022-10-08 20:24:00 +0300

    Added add task1

---
+++ dz1/task_additional01.py
@@ -0,0 +1,28 @@
+# time = int(input("Input time in seconds: "))
+time_list = [53, 153, 4153, 400153]
+
+seconds = minutes = hours = days = 0
+
+for time in time_list:
+    if time < -1:
+        print("Wrong time")
+        quit()
+
+    if time < 60:
+        seconds = time
+        print(f"{seconds}s")
+    elif time < 3600:
+        minutes = int(time / 60)
+        seconds = time % 60
+        print(f"{minutes}m {seconds}s")
+    elif time < 86400:
+        hours = int(time / 3600)
+        minutes = int(time % 3600 / 60)
+        seconds = time % 3600 % 60
+        print(f"{hours}h {minutes}m {seconds}s")
+    else:
+        days = int(time / 86400)
+        hours = int(time % 86400 / 3600)
+        minutes = int(time % 86400 % 3600 / 60)
+        seconds = time % 86400 % 3600 % 60
+        print(f"{days}d {hours}h {minutes}m {seconds}s")

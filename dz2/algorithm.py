f304bd58a5d295a9b2fb16711651d2c12da8cae9
Artem <milkartem2@mail.ru>
2022-10-13 23:37:38 +0300

    Added algorythm

---
+++ dz2/algorithm.py
@@ -0,0 +1,14 @@
+from random import randint
+
+list1 = [randint(10) for i in range(10)]
+list2 = [randint(10) for i in range(10)]
+
+list_cross=[]
+
+for i in range(len(list1)):
+    if list1[i] in list2 or list1[i] in list_cross:
+        list_cross.append(list1[i])
+
+print(list1)
+print(list2)
+print(list_cross)

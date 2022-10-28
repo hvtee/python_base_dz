666c2b6b782d947863ed6866016a6d718070d75c
Artem <milkartem2@mail.ru>
2022-10-28 20:25:14 +0300

    Game pve added

--- dz5/sweets_game.py
+++ dz5/sweets_game.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 
@@ -40,9 +41,7 @@ def game_pvp():
         return sweets_
 
     print()
-    print()
-    print()
-    print("Your game type: pvp")
+    print("Your game type: pve")
 
     while True:  # PLAYERS QUANTITY
         players_quantity = int(input("Input players quantity(2-4): "))
@@ -54,8 +53,88 @@ def game_pvp():
     players = input_players_name(players_quantity)
     current_player_index = find_first_player(players)
 
-    sweets = int(input("How many sweets do you want? "))
-    while sweets > 0: # MAIN GAME
+    while True:  # SWEETS QUANTITY
+        sweets = int(input("How many sweets do you want(112-2022)? "))
+        if 112 <= sweets <= 2022:
+            break
+        print("Wrong!")
+        print()
+
+    while sweets > 0:  # MAIN GAME
+        sweets = game_turn(players, current_player_index, sweets)
+
+        if sweets == 0:
+            print()
+            print()
+            print()
+
+            print(f"Current sweets: {sweets}")
+            print(f'Player {players[current_player_index]} has won!')
+            break
+
+        if current_player_index == len(players) - 1:
+            current_player_index = 0
+        else:
+            current_player_index += 1
+
+
+def game_pve():
+    def game_turn(players_, current_player_index_, sweets_):
+        print()
+        time.sleep(1)
+        if current_player_index_ == len(players_) - 1:
+            while True:
+                print(f"Current sweets: {sweets_}")
+                sweets_to_take = int(input(f"{players_[current_player_index_]}"
+                                           f", how many sweets do you want to take(1-28): "))
+                if 1 <= sweets_to_take <= 28 and sweets_ - sweets_to_take >= 0:
+                    sweets_ -= sweets_to_take
+                    break
+                print()
+                print("Wrong sweets value")
+                print()
+        else:
+            print(f"Current sweets: {sweets_}")
+            if sweets_ > 28:
+                sweets_to_take = randint(1, 28)
+                sweets_ -= sweets_to_take
+
+                print(f"{players_[current_player_index_]}"
+                      f", how many sweets do you want to take(1-28): {sweets_to_take}")
+            else:
+                sweets_to_take = sweets_
+
+                print(f"{players_[current_player_index_]}"
+                      f", how many sweets do you want to take(1-28): {sweets_to_take}")
+
+                sweets_ -= sweets_to_take
+        return sweets_
+
+    bot_names = ['Bob', 'Karl', 'Dan', 'Mike', 'Lad', 'John', 'Jason', 'Peter', 'Patrik']
+    while True:  # BOTS QUANTITY
+        bots_quantity = int(input("Input bots quantity(1-3): "))
+        if 0 < bots_quantity < 4:
+            break
+        print("Wrong!")
+        print()
+
+    players = []  # BOT NAMES
+    while len(players) != 3:
+        bot_name_index = randint(0, len(bot_names) - 1)
+        if bot_names[bot_name_index] not in players:
+            players.append(bot_names[bot_name_index])
+    print(f'Bot names: {players}')
+    players.append(input("Input your name: "))
+
+    current_player_index = find_first_player(players)
+    while True:  # SWEETS QUANTITY
+        sweets = int(input("How many sweets do you want(112-2022)? "))
+        if 112 <= sweets <= 2022:
+            break
+        print("Wrong!")
+        print()
+
+    while sweets > 0:  # MAIN GAME
         sweets = game_turn(players, current_player_index, sweets)
 
         if sweets == 0:
@@ -74,33 +153,52 @@ def game_pvp():
 
 
 def main():
-    game_type = choose_game_type()
+    print()
+    print("Hello, lets play sweet game!")
+    print()
 
-    if game_type == 1:  # PLAYER VS PLAYER
-        while True:
+    while True:
+        game_type = choose_game_type()
 
-            game_pvp()
-
+        if game_type == 1:  # PLAYER VS PLAYER
             while True:
-                print()
-                print()
-                print()
+                game_pvp()
 
-                choose = int(input("Do you want to play again? 1. Yes 2. No: "))
-                if choose == 1 or choose == 2:
+                while True:
+                    print()
+                    choose = int(input("Back? 1. No 2. Yes | "))
+                    if choose == 1 or choose == 2:
+                        break
+                    print("Wrong!")
+                if choose == 2:
                     break
-                print("Wrong!")
-            if choose == 2:
-                print()
-                print()
-                print()
+        elif game_type == 2:  # PLAYER VS BOT
+            while True:
+                game_pve()
 
-                print('Thanks playing!')
+                while True:
+                    print()
+                    choose = int(input("Back? 1. No 2. Yes | "))
+                    if choose == 1 or choose == 2:
+                        break
+                    print("Wrong!")
+                if choose == 2:
+                    break
+
+        while True:
+            print()
+            choose = int(input("Do you want to play again? 1. Yes 2. No | "))
+            if choose == 1 or choose == 2:
+                print('Thanks for playing!')
                 break
+            print("Wrong!")
+        if choose == 2:
+            print()
+            print()
+            print()
 
-    elif game_type == 2:  # PLAYER VS BOT
-        quit()
-        players = input("Input player name: ")
+            print('Thanks playing!')
+            break
 
 
 main()
